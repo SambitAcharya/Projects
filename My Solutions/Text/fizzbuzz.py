e096c23b5e6981ad67cd2b834e1b605d083d0f42
SambitAcharya <sambitacharya121@gmail.com>
2015-02-25 19:50:25 +0530

    Added comments

--- My Solutions/Text/fizzbuzz.py
+++ My Solutions/Text/fizzbuzz.py
@@ -1,3 +1,15 @@
+'''
+Created For Mega Projects Repository
+
+Problem: Write a program that prints the numbers from 1 to 100.
+But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+For numbers which are multiples of both three and five print “FizzBuzz”
+
+@author: Sambit
+
+'''
+
+
 def FizzBuzz(n):
     if n % 3 == 0 and n % 5 == 0:
         return 'FizzBuzz'
@@ -7,3 +19,5 @@ def FizzBuzz(n):
         return 'Buzz'
     else:
         return str(n)
+
+print "\n".join(FizzBuzz(n) for n in xrange(1, 101))
