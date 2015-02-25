7ac58f1fb1c4743bf1685a941cce1ae93fd52658
SambitAcharya <sambitacharya121@gmail.com>
2015-02-25 20:48:47 +0530

    Added comments

--- My Solutions/Text/countvowels.py
+++ My Solutions/Text/countvowels.py
@@ -1,3 +1,13 @@
+'''
+Created For Mega Projects Repository
+
+Problem: Enter a string and the program counts the number of vowels in the text.
+For added complexity have it report a sum of each vowel found.
+
+@author: Sambit
+
+'''
+
 word = raw_input("Enter a sentence \n").lower()
 vowels = "aeiou"
 count_total = 0
